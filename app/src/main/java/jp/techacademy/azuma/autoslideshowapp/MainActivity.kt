02f50294169f865c3a822b5270a0ec52ef9143dc
battery3210 <ring321@yahoo.co.jp>
2021-10-10 23:37:47 +0900

    2番目のコミット、進むボタン、戻るボタン途中まで完成

--- app/src/main/java/jp/techacademy/azuma/autoslideshowapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/azuma/autoslideshowapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package jp.techacademy.azuma.autoslideshowapp
 
 import android.Manifest
+import android.content.ContentResolver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.pm.PackageManager
@@ -8,56 +9,43 @@ import android.os.Build
 import android.util.Log
 import android.provider.MediaStore
 import android.content.ContentUris
+import android.database.Cursor
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 import android.os.Handler
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-    private val PERMISSIONS_REQUEST_CODE = 100 //※質問 紫色になる意味
+    private val PERMISSIONS_REQUEST_CODE = 100 //
+
+    //画像の情報を取得
+    var cursor: Cursor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var mTimer: Timer? = null //※質問　コロンの意味
+        var mTimerSec = 0.0
+        var mHandler = Handler() //
+
         //6.0以降の場合
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
             if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
                 //許可されている
-                getContentsInfo()
+                //getContentsInfo()
             } else {
                 //許可されてないのでダイアログ表示
                 requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),PERMISSIONS_REQUEST_CODE) //ここを質問、何をしているのか　配列？
             }
         } else {
-            getContentsInfo()
+            //getContentsInfo()
         }
 
-        start_button.setOnClickListener {
-            getContentsInfo()
-        }
+        val resolver = contentResolver
 
-
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String> ,grantResults: IntArray){
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            PERMISSIONS_REQUEST_CODE ->
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                    getContentsInfo()
-                }
-        }
-    }
-
-
-    private fun getContentsInfo(){
-        var mTimer: Timer? = null //※質問　コロンの意味
-        var mTimerSec = 0.0
-        var mHandler = Handler() //※質問　ハンドラーの上に線が引かれる
-        //画像の情報を取得
-        val resolver = contentResolver //色が紫になるのはどういう意味なのか？
-        val cursor = resolver.query(
+        cursor = resolver.query(
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI, //データの種類
             null,
             null,
@@ -65,17 +53,76 @@ class MainActivity : AppCompatActivity() {
             null
         )
 
+        susumu_button.setOnClickListener {
+            //getNextInfo()
+            // timer.text = String.format("%.1f", mTimerSec)
+            //indexからIDを取得、IDから画像のURI取得
+            cursor!!.moveToNext()
+            //val nextCusor = cursor.moveToNext()
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id)
+
+            //Log.d("ANDROID_TEST","URI:" + imageUri.toString())
+            ImageView.setImageURI(imageUri)
+            //var next = cursor.moveToNext()
+            //Log.d("ANDROID_TEST","URI:" + imageUri.toString())
+            Log.d("ANDROID_TETE","URI:" + imageUri.toString())
+        }
+
+
+
+
+
+        //start_stop.setOnClickListener(this)
+        modoru_button.setOnClickListener {
+            //getContentsInfo()
+            //getNextInfo()
+            // timer.text = String.format("%.1f", mTimerSec)
+            //indexからIDを取得、IDから画像のURI取得
+            cursor!!.moveToPrevious()
+            //val nextCusor = cursor.moveToNext()
+            val fieldIndex = cursor!!.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor!!.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id)
+
+            //Log.d("ANDROID_TEST","URI:" + imageUri.toString())
+            ImageView.setImageURI(imageUri)
+            //var next = cursor.moveToNext()
+            //Log.d("ANDROID_TEST","URI:" + imageUri.toString())
+            Log.d("ANDROID_TETE","URI:" + imageUri.toString())
+        }
+
+    }
+
+/*
+    private fun getNextInfo(){ //進むボタン用
+
+            // timer.text = String.format("%.1f", mTimerSec)
+            //indexからIDを取得、IDから画像のURI取得
+            cursor.moveToNext()
+            //val nextCusor = cursor.moveToNext()
+            val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+            val id = cursor.getLong(fieldIndex)
+            val imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,id)
+
+            //Log.d("ANDROID_TEST","URI:" + imageUri.toString())
+            ImageView.setImageURI(imageUri)
+            //var next = cursor.moveToNext()
+            //Log.d("ANDROID_TEST","URI:" + imageUri.toString())
+            Log.d("ANDROID_TETE","URI:" + imageUri.toString())
+
+        }
+        cursor.close()
+        //mTimer?.cancel()
+    }
+
+
+    private fun getContentsInfo(){  //再生ボタン用
+
 
         if(cursor!!.moveToFirst()){
 
-            if (mTimer == null) {
-                //タイマーの作成
-                mTimer = Timer()
-                //タイマーの始動
-                mTimer!!.schedule(object : TimerTask() {
-                    override fun run() {
-                        mTimerSec += 0.1
-                        mHandler.post {
                             // timer.text = String.format("%.1f", mTimerSec)
                             //indexからIDを取得、IDから画像のURI取得
                             val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
@@ -85,13 +132,26 @@ class MainActivity : AppCompatActivity() {
                             //Log.d("ANDROID_TEST","URI:" + imageUri.toString())
                             ImageView.setImageURI(imageUri)
                             Log.d("ANDROID_TEST","URI:" + imageUri.toString())
-                        } //どこでループさせているのかを質問する
-                    }
-                }, 100, 100) //最初に始動させるまで100ミリ秒、ループの間隔を100ミリ秒に設定
-            }
+
         }
         cursor.close()
-        mTimer?.cancel()
+        //mTimer?.cancel()
+    }
+
+ */
+
+
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,permissions: Array<String> ,grantResults: IntArray){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            PERMISSIONS_REQUEST_CODE ->
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //getContentsInfo()
+                }
+        }
     }
 
 
